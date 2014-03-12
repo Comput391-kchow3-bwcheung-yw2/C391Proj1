f7f2a5245af46818eb2816b279e47c2aed0414e3
Brandon Cheung <bwcheung@cs.ualberta.ca>
2014-03-12 15:04:54 -0600

    Finished management pages.

--- Modify.php
+++ Modify.php
@@ -3,18 +3,67 @@
 <h1> Modify Data </h1>
 <body>
 	<?php
-		if (isset($_POST['update'])) {
-			echo "<form action = 'Users.php' METHOD = 'POST'></br>";
-			echo 'Put in the person\'s followed by the information you would like to update';
-			echo "ID: <input type = 'number' name = 'id'/></br>";
+		if (isset($_POST['Update_Users'])) {
+			echo "<form action = 'managment.php' METHOD = 'POST'></br>";
+			echo 'Put in the users\'s id followed by the information you would like to update';
+			echo "ID: <input type = 'text' name = 'id'/></br>";
 			echo "Username: <input type = 'text' name = 'user'/></br>";
 			echo "Password: <input type = 'text' name = 'pass'/></br>";
 			echo "Class: <input type = 'text' name = 'class'/></br>";
-			echo "Date: <input type = 'text' name = 'date'/></br>";
-			echo "<input type = 'submit' name = 'update' value = 'update'/>";
+			echo "Date Registered: <input type = 'text' name = 'date'/></br>";
+			echo "<input type = 'submit' name = 'update_users' value = 'update'/>";
 			echo "</form>";
 		}
-		if (isset($_POST['enter'])) {
+		if (isset($_POST['Enter_Users'])) {
+			echo "<form action = 'managment.php' METHOD = 'POST'></br>";
+			echo 'Put in the new users\'s information you would like to enter:</br>';
+			echo "ID: <input type = 'text' name = 'id'/></br>";
+			echo "Username: <input type = 'text' name = 'user'/></br>";
+			echo "Password: <input type = 'text' name = 'pass'/></br>";
+			echo "Class: <input type = 'text' name = 'class'/></br>";
+			echo "Date Registered: <input type = 'text' name = 'date'/></br>";
+			echo "<input type = 'submit' name = 'enter_users' value = 'enter'/>";
+			echo "</form>";
+		}
+		if (isset($_POST['Update_Doctors'])) {
+			echo "<form action = 'managment.php' METHOD = 'POST'></br>";
+			echo 'Put in the doctor\'s id followed by the information you would like to update';
+			echo "ID: <input type = 'text' name = 'id'/></br>";
+			echo "Patient ID: <input type = 'text' name = 'p_id'/></br>";
+			echo "<input type = 'submit' name = 'update_doctors' value = 'update'/>";
+			echo "</form>";
+		}
+		if (isset($_POST['Enter_Doctors'])) {
+			echo "<form action = 'managment.php' METHOD = 'POST'></br>";
+			echo 'Put in the new doctor\'s information you would like to enter:</br>';
+			echo "Doctor ID: <input type = 'text' name = 'id'/></br>";
+			echo "Patient ID: <input type = 'text' name = 'p_id'/></br>";
+			echo "<input type = 'submit' name = 'enter_doctors' value = 'enter'/>";
+			echo "</form>";
+		}
+		if (isset($_POST['Update_Persons'])) {
+			echo "<form action = 'managment.php' METHOD = 'POST'></br>";
+			echo 'Put in the person\'s id followed by the information you would like to update';
+			echo "ID: <input type = 'text' name = 'id'/></br>";
+			echo "First Name: <input type = 'text' name = 'first'/></br>";
+			echo "Last Name: <input type = 'text' name = 'last'/></br>";
+			echo "Address: <input type = 'text' name = 'address'/></br>";
+			echo "Email: <input type = 'text' name = 'email'/></br>";
+			echo "Phone: <input type = 'text' name = 'phone'/></br>";
+			echo "<input type = 'submit' name = 'update_persons' value = 'update'/>";
+			echo "</form>";
+		}
+		if (isset($_POST['Enter_Persons'])) {
+			echo "<form action = 'managment.php' METHOD = 'POST'></br>";
+			echo 'Put in the new person\'s information you would like to enter:</br>';
+			echo "ID: <input type = 'text' name = 'id'/></br>";
+			echo "First Name: <input type = 'text' name = 'first'/></br>";
+			echo "Last Name: <input type = 'text' name = 'last'/></br>";
+			echo "Address: <input type = 'text' name = 'address'/></br>";
+			echo "Email: <input type = 'text' name = 'email'/></br>";
+			echo "Phone: <input type = 'text' name = 'phone'/></br>";
+			echo "<input type = 'submit' name = 'enter_persons' value = 'enter'/>";
+			echo "</form>";
 		}
 	?>
 </center>
---
+++ managment.php
@@ -0,0 +1,214 @@
+<html>
+    <body>
+	<center>
+        <?php 
+	   include ("PHPconnectionDB.php");
+           if (isset($_POST['enter_users'])) {
+		$conn=connect();
+		$user = $_POST['user'];
+		$pass = $_POST['pass'];
+		$id = $_POST['id'];
+		$class = $_POST['class'];
+		$date = $_POST['date'];
+		$sql = 'INSERT INTO USERS VAlUES (\''.$user.'\''.', \''.$pass.'\''.', \''.$class.'\''.', \''.$id.'\''.', \''.$date.'\''.')';
+		$stid = oci_parse($conn, $sql);
+           	$res=oci_execute($stid); 
+           	if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+           	}
+                Users_table();
+	   }
+	   if (isset($_POST['enter_persons'])) {
+		$conn = connect();
+		$first = $_POST['first'];
+		$last = $_POST['last'];
+		$id = $_POST['id'];
+		$address = $_POST['address'];
+		$phone = $_POST['phone'];
+		$email = $_POST['email'];
+		$sql = 'INSERT INTO PERSONS VAlUES (\''.$id.'\''.', \''.$first.'\''.', \''.$last.'\''.', \''.$address.'\''.', \''.$email.'\''.',\''.$phone.'\''.')';
+		$stid = oci_parse($conn, $sql);
+           	$res=oci_execute($stid); 
+           	if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+           	}
+                Persons_table();
+	   }
+	   if (isset($_POST['enter_doctors'])) {
+		$conn = connect();
+		$id = $_POST['id'];
+		$p_id = $_POST['p_id'];
+		$sql = 'INSERT INTO FAMILY_DOCTOR VAlUES (\''.$id.'\''.', \''.$p_id.'\''.')';
+		$stid = oci_parse($conn, $sql);
+           	$res=oci_execute($stid); 
+           	if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+           	}
+                Doctors_table();
+	   }
+           if (isset($_POST['update_users'])) {
+		 $conn = connect();
+		$user = $_POST['user'];
+		$pass = $_POST['pass'];
+		$id = $_POST['id'];
+		$class = $_POST['class'];
+		$date = $_POST['date'];
+		$sql = 'UPDATE users SET USER_NAME = \''.$user.'\''.', PASSWORD = \''.$pass.'\''.', CLASS = \''.$class.'\''.', DATE_REGISTERED = \''.$date.'\''.' WHERE  PERSON_ID = \''.$id.'\'';
+          	$stid = oci_parse($conn, $sql);
+           	$res=oci_execute($stid); 
+           	if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+           	}
+                Users_table();
+           }
+	   if (isset($_POST['update_persons'])) {
+		$conn = connect();
+		$first = $_POST['first'];
+		$last = $_POST['last'];
+		$id = $_POST['id'];
+		$address = $_POST['address'];
+		$phone = $_POST['phone'];
+		$email = $_POST['email'];
+		$sql = 'UPDATE persons SET FIRST_NAME = \''.$first.'\''.', LAST_NAME = \''.$last.'\''.', ADDRESS = \''.$address.'\''.', PHONE = \''.$phone.'\''.', EMAIL = \''.$email.'\''.' WHERE  PERSON_ID = \''.$id.'\'';
+          	$stid = oci_parse($conn, $sql);
+           	$res=oci_execute($stid); 
+           	if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+           	}
+		Persons_table();
+           }
+	   if (isset($_POST['update_doctors'])) {
+		$conn = connect();
+		$id = $_POST['id'];
+		$p_id = $_POST['p_id'];
+		$sql = 'UPDATE family_doctor SET PATIENT_ID = \''.$p_id.'\''.' WHERE  DOCTOR_ID = \''.$id.'\'';
+          	$stid = oci_parse($conn, $sql);
+           	$res=oci_execute($stid); 
+           	if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+           	}
+		Doctors_table();
+           }
+	   if (isset($_POST['Users'])) {
+		users_table();
+	   }
+	  
+	    if (isset($_POST['Persons'])) {
+			Persons_table();
+	    }
+	    if (isset($_POST['Doctors'])) {
+		Doctors_table();
+	    }
+	    function Doctors_table() {
+		   $conn = connect();
+		   echo '<h1> Doctors Table </h1>';
+		   echo '<FORM action = "Modify.php" Method = "post"></br>';
+	           echo 'To update table: <input type = "submit" name = "Update_Doctors" value = "submit"/></br>';
+		   echo 'To enter new data: <input type = "submit" name = "Enter_Doctors" value = "submit"/></br>';
+ 	           echo '</form>';          
+		   echo '<table>';
+		   echo '<tr>';
+		   echo '<th> Doctor ID </th>';
+		   echo '<th> Patient ID </th>';
+		   echo '</tr>';  
+		   $sql = 'SELECT * FROM family_doctor';
+		   $stid = oci_parse($conn, $sql );
+		   $res=oci_execute($stid); 
+		   if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+		   }
+		   while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+		   	echo '<tr>';
+			foreach ($row as $item) {
+				echo "<td> $item </td>";
+			}
+			echo '</tr>';
+			echo '<br/>';
+		   }
+		   echo '</table>';
+		   oci_free_statement($stid);
+		   oci_close($conn);
+	    }
+           function Persons_table() {       
+                   $conn=connect();
+           	   echo '<h1> Persons Table </h1>';
+		   echo '<FORM action = "Modify.php" Method = "post"></br>';
+	           echo 'To update table: <input type = "submit" name = "Update_Persons" value = "submit"/></br>';
+		   echo 'To enter new data: <input type = "submit" name = "Enter_Persons" value = "submit"/></br>';
+ 	           echo '</form>';        
+		   echo '<table>';
+		   echo '<tr>';
+		   echo '<th> ID </th>';
+		   echo '<th> First Name </th>';
+		   echo '<th> Last Name </th>';
+		   echo '<th> Address </th>';
+		   echo '<th> Email </th>';
+		   echo '<th> phone </th>';
+		   echo '</tr>';  
+		   $sql = 'SELECT * FROM persons';
+		   $stid = oci_parse($conn, $sql );
+		   $res=oci_execute($stid); 
+		   if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+		   }
+		   while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+		   	echo '<tr>';
+			foreach ($row as $item) {
+				echo "<td> $item </td>";
+			}
+			echo '</tr>';
+			echo '<br/>';
+		   }
+		   echo '</table>';
+		   oci_free_statement($stid);
+		   oci_close($conn);
+	    }
+            function Users_table() {
+		   $conn=connect();
+		   echo '<h1> Users Table </h1>';
+		   echo '<FORM action = "Modify.php" Method = "post"></br>';
+	           echo 'To update table: <input type = "submit" name = "Update_Users" value = "submit"/></br>';
+		   echo 'To enter new data: <input type = "submit" name = "Enter_Users" value = "submit"/></br>';
+ 	           echo '</form>';    
+		   echo '<table>';
+		   echo '<tr>';
+		   echo '<th> Username </th>';
+		   echo '<th> Password </th>';
+		   echo '<th> Class </th>';
+		   echo '<th> ID </th>';
+		   echo '<th> Date </th>';
+		   echo '</tr>';  
+		   $sql = 'SELECT * FROM Users';
+		   $stid = oci_parse($conn, $sql );
+		   $res=oci_execute($stid); 
+		   if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+		   }
+		   while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+		   	echo '<tr>';
+			foreach ($row as $item) {
+				echo "<td> $item </td>";
+			}
+			echo '</tr>';
+			echo '<br/>';
+		   }
+		   echo '</table>';
+		   oci_free_statement($stid);
+		   oci_close($conn);
+	    }
+	    echo '<FORM action = "managment.html" Method = "post"></br>';
+	    echo 'Go back to managment home page: <input type = "submit" name = "submit" value = "back"/></br>';  
+	?>
+	</table>
+	</center>
+    </body>
+</html>
