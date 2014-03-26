8b6864c19ee0ed99e8416d520c32e792e77f956b
kchow3 <kchow3@ualberta.ca>
2014-03-26 09:37:28 -0600

    Fixed report.

--- report.php
+++ report.php
@@ -20,33 +20,41 @@
 		echo '<th> Phone </th>';
 		echo '<th> Test Date </th>';
 		echo '</tr>';
-
-		//sql command
-		$sql = 'SELECT persons.first_name, persons.last_name, persons.address, persons.phone, radiology_record.test_date
-			FROM persons, users, radiology_record
-			WHERE users.class = 'a', persons.person_id = radiology_record.patient_id, radiology_record.diagnosis = \''.$diagnosis.'\', radiology_record.test_date > \''.$from.'\', radiology_record.test_date < \''.$till.'\'';
 		
-		//Prepare sql using conn and returns the statement identifier
-		$stid = oci_parse($conn, $sql);
-		
-		//Execute a statement returned from oci_parse()
-		$res = oci_execute($stid);
-		
-		//if error, retrieve the error using the oci_error() function & output an error
-		if(!$res)
+		if($from != "" && $till != "")
 		{
-			$err = oci_error($stid);
-			echo htmlentities($err['message']);
+		    $time_period = 'TO_DATE(\''.$from.'\', \'yyyymmdd\') AND TO_DATE(\''.$till.'\', \'yyyymmdd\')';
+
+		    //sql command
+		    $sql = 'SELECT persons.first_name, persons.last_name, persons.address, persons.phone, radiology_record.test_date
+			    FROM persons, users, radiology_record
+			    WHERE persons.person_id = radiology_record.patient_id AND radiology_record.diagnosis = \''.$diagnosis.'\' AND radiology_record.test_date BETWEEN '.$time_period;
+		    
+		    //Prepare sql using conn and returns the statement identifier
+		    $stid = oci_parse($conn, $sql);
+		    
+		    //Execute a statement returned from oci_parse()
+		    $res = oci_execute($stid);
+		    
+		    //if error, retrieve the error using the oci_error() function & output an error
+		    if(!$res)
+		    {
+			    $err = oci_error($stid);
+			    echo htmlentities($err['message']);
+		    }
+		    
+		    while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+			    echo '<tr>';
+			    foreach ($row as $item) {
+				    echo $item.'&nbsp;';
+			    }
+			    echo '</tr>';
+			    echo '<br/>';
+		    }
 		}
-		
-		while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
-	   		echo '<tr>';
-			foreach ($row as $item) {
-				echo $item.'&nbsp;';
-			}
-			echo '</tr>';
-			echo '<br/>';
-	   	}
+		else{
+		    echo 'No time period entered.';
+		}
 		
 		echo '</table>';
 		oci_free_statement($stid);
